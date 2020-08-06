0c1059e7c441ae58828ea0e453d8cc7c2a81e82f
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-08-06 15:40:24 -0700

    Initial commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Anmol Raibhandare on 8/6/20.
+//  Copyright © 2020 Anmol Raibhandare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
