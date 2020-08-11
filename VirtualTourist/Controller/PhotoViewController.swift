af09b454446827cad17ef114b2d307145db9f7ae
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-08-11 12:55:30 -0700

    feat: Implemented core data stack in photoViewController.

--- VirtualTourist/Controller/PhotoViewController.swift
+++ VirtualTourist/Controller/PhotoViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 import CoreData
 import MapKit
 
-class PhotoViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+class PhotoViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -79,7 +80,7 @@ class PhotoViewController: UIViewController, UICollectionViewDelegateFlowLayout,
         
         
         collectionView.delegate = self
-//        collectionView.dataSource = self
+        collectionView.dataSource = self
         
         newCollection.isHidden = false
         
@@ -102,12 +103,119 @@ class PhotoViewController: UIViewController, UICollectionViewDelegateFlowLayout,
     }
     
     func displayNewResult(){
+        newCollection.isEnabled = false
+        deleteExisting()
+        photos.removeAll()
+        collectionView.reloadData()
         
+        getFlickrImages{ (images) in
+            if images != nil {
+                DispatchQueue.main.async {
+                    self.addData(flickrImages: images!, coreDataPin: self.pin)
+                    self.photos = self.loadSavedPhotos()!
+                    self.displaySavedResult()
+                    self.newCollection.isEnabled = true
+                }
+            }
+        }
+    }
+    
+    func addData(flickrImages: [FlickrImage], coreDataPin: Pin){
+        for image in flickrImages {
+            do{
+                let delegate = UIApplication.shared.delegate as! AppDelegate
+                let dataController = delegate.dataController
+                let photo = Photo(index: flickrImages.index{$0 === image}!, imageURL: image.imageURL(), imageData: nil, context: dataController.context)
+                try dataController.autoSaveViewContext()
+            } catch {
+                print("Add Failed")
+            }
+        }
+    }
+    
+    func deleteExisting() {
+        for photo in photos {
+            setUpCoreDataStack().context.delete(photo)
+        }
+    }
+    
+    func getFlickrImages(completion: @escaping (_ result: [FlickrImage]?) -> Void) {
+        
+        var resultImages:[FlickrImage] = []
+        FlickrAPI.getImage(latitude: coordinate.latitude, longitude: coordinate.longitude) { (success, flickrImages) in
+            if success {
+                if flickrImages!.count > self.totalCells {
+                    var temp:[Int] = []
+                    
+                    while temp.count < self.totalCells {
+                        let random = arc4random_uniform(UInt32(flickrImages!.count))
+                        if !temp.contains(Int(random)) {
+                            temp.append(Int(random))
+                        }
+                    }
+                    for random in temp {
+                        resultImages.append(flickrImages![random])
+                    }
+                    completion(resultImages)
+                } else {
+                    completion(flickrImages!)
+                }
+            } else {
+                completion(nil)
+            }
+        }
     }
     
     func addAnnotation() {
-        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations([annotation], animated: true)
     }
+    
+    
+    @IBAction func newCollectionButtonResult(_ sender: Any) {
+        
+        if toDelete.count > 0 {
+            removePhotos()
+            unselectAll()
+            photos = loadSavedPhotos()!
+            displaySavedResult()
+        } else {
+            displayNewResult()
+        }
+    }
+    
+    func unselectAll() {
+        for index in collectionView.indexPathsForSelectedItems! {
+            collectionView.deselectItem(at: index, animated: false)
+            collectionView.cellForItem(at: index)?.contentView.alpha = 1
+        }
+    }
+    
+    func removePhotos() {
+        for index in 0..<photos.count {
+            if(toDelete.contains(index)) {
+                setUpCoreDataStack().context.delete(photos[index])
+            }
+        }
+        do {
+            try setUpCoreDataStack().autoSaveViewContext()
+        } catch {
+            print("Remove Failed")
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
 
 
 }
